8fc52f9acb09884bbd8bac807390f539b75b3ee2
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-03-24 13:50:55 +0100

    Sperimentando cose

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// 1. chiedi all’utente il cognome
+// 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// 3. stampa la lista ordinata alfabeticamente
+// 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente 
+// si trova (facciamo questo in 2 versioni: col ciclo for e col while)
+
+
+// Creo un Array con i cognomi
+var arrayCognomi = ["Faragli", "Totti", "Ginanneschi", "Papparapa"];
+
+// Chiedo all'utente di inserire il suo cognome
+var cognomeUtente = prompt('Inserisci il tuo cognome: ');
+
+// Inserisco il cognome dell'utente nell'Array
+arrayCognomi.push(cognomeUtente);
+
+// Ordino i cognomi in ordine alfabetico
+arrayCognomi.sort();
+
+console.log(arrayCognomi)
